085d1b3bfd4ed2ec0ccbf5f5ebd002e149e9f7e0
AlejoXDeath <alejoferrada473@gmail.com>
2024-11-04 11:01:29 -0300

    configure el faq2

--- src/app/modules/producto/producto-routing.module.ts
+++ src/app/modules/producto/producto-routing.module.ts
@@ -7,6 +7,7 @@ import { SoporteComponent } from './pages/soporte/soporte.component';
 import { DlcComponent } from './pages/dlc/dlc.component';
 import { SoundtrackComponent } from './pages/soundtrack/soundtrack.component';
 import { Faq1Component } from './pages/faq1/faq1.component';
+import { Faq2Component } from './pages/faq2/faq2.component';
 
 
 const routes: Routes = [
@@ -27,6 +28,9 @@ const routes: Routes = [
   },
   {
     path:"faq1",component:Faq1Component
+  },
+  {
+    path:"faq2",component:Faq2Component
   }
 ];
 
